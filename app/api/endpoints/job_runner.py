535879a4ec9fdba71acf026fe9953eafe40c870a
Dastan1997 <sld377575@gmail.com>
2024-07-18 12:57:26 -0700

    feature-999: update job_executor

--- app/api/endpoints/job_runner.py
+++ app/api/endpoints/job_runner.py
@@ -1,14 +1,13 @@
-from fastapi import Depends, APIRouter
-from typing_extensions import Annotated
+from fastapi import APIRouter
 
-from app.models.schemas import TaskRequest, TaskResponse
-from app.service.task_executor import TaskExecutor
-from app.api.deps import get_task_executor
+from app.models.schema import JobRequest, JobResponse
+from app.services.job_executor import JobExecutor
 
 router = APIRouter()
 
 
-@router.post("/execute_task", response_model=TaskResponse)
-async def execute_task(task: TaskRequest, task_executor: Annotated[TaskExecutor, Depends(get_task_executor)]):
-    result = await task_executor.execute(task)
-    return TaskResponse(status="success", output=result)
+@router.post("/job", response_model=JobResponse)
+async def execute_task(job: JobRequest):
+    result = await JobExecutor.factory(resources=job.resources) \
+        .execute(job.code)
+    return JobResponse(status="success", output=result)
--- app/services/job_executor.py
+++ app/services/job_executor.py
@@ -1,14 +1,23 @@
 import asyncio
-from typing import Annotated
-
+import aiodocker
 
 from app.models.schema import JobRequest
 
-class JobExecutor:
-    def __init__(self, resource):
-        self.resource = resource
 
-    async def execute(self, task: JobRequest) -> str:
+class JobExecutor:
+    def __init__(self, resources):
+        self.resources = resources
+        self.docker_client = aiodocker.Docker()
+    def setup_docker_volume(self):
+        pass
+    def setup_docker_container(self):
+        pass
+
+    @classmethod
+    def factory(cls, resources):
+        return cls(resources=resources)
+
+    async def execute(self, code: str) -> str:
         """
         execute task inside docker container with resources specified in task.
         :param task:
